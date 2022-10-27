6169a37a97afb1cac1a3acdabb43b22d4fa73536
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-10-27 10:08:35 -0300

    30% de cobertura :rocket: ?

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,68 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
+import mockFetch from '../../cypress/mocks/fetch';
+import renderWithRouter from './helpers/renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import { toBeInTheDocument, toBeInTheDOM } from '@testing-library/jest-dom/dist/matchers';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testes do StarWars', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(mockFetch);
+  });
+
+  afterEach(() => {
+    global.fetch.mockClear();
+  });
+
+  it('01 - testa a requisição e o filtro por nome', async () => {
+    const { history } = renderWithRouter(<App/>);
+    expect(history.location.pathname).toBe('/');
+
+    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+
+    const nameFilter = screen.getByTestId('name-filter');
+    expect(screen.getByTestId('name-filter')).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText(/tatooine/i)).toBeInTheDocument());
+    const tatooine = screen.getByText(/tatooine/i);
+    userEvent.type(nameFilter, 'al');
+    expect(tatooine).not.toBeInTheDocument()
+
+  })
+  it('02 - testa o filtro por coluna', async () => {
+    const { history } = renderWithRouter(<App/>);
+    expect(history.location.pathname).toBe('/');
+
+    await waitFor(() => expect(screen.getByText(/tatooine/i)).toBeInTheDocument());
+
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter')
+    const valueFilter = screen.getByTestId('value-filter');
+    const buttonFilter = screen.getByTestId('button-filter');
+    const tatooine = screen.getByText(/tatooine/i);
+
+ 
+
+    userEvent.selectOptions(columnFilter, 'diameter');
+    userEvent.selectOptions(comparisonFilter, 'igual a');
+    userEvent.type(valueFilter, '4900');
+    userEvent.click(buttonFilter);
+    expect(tatooine).not.toBeInTheDocument();
+
+
+    
+  })
+  it('', () => {
+    
+  })
+  it('', () => {
+    
+  })
+  it('', () => {
+    
+  })
+  it('', () => {
+    
+  })
+
+})
